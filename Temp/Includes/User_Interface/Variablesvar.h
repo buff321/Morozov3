edfc1e48a3763ed0e3c6f41f08d7c463dfd5ba4d
You <56828327+Vitaly-Gal@users.noreply.github.com>
2023-12-25 13:07:13 +0300

    firstCom

---
+++ Temp/Includes/User_Interface/Variablesvar.h
@@ -0,0 +1,36 @@
+/* Automation Studio generated header file */
+/* Do not edit ! */
+
+#ifndef _BUR_1621514705_8_
+#define _BUR_1621514705_8_
+
+#include <bur/plctypes.h>
+
+/* Constants */
+#ifdef _REPLACE_CONST
+#else
+#endif
+
+
+/* Variables */
+_BUR_LOCAL struct MC_Power power;
+_BUR_LOCAL struct MC_Home homing;
+_BUR_LOCAL unsigned short length;
+
+
+
+
+
+__asm__(".section \".plc\"");
+
+/* Used IEC files */
+__asm__(".ascii \"iecfile \\\"Logical/User_Interface/Variables.var\\\" scope \\\"local\\\"\\n\"");
+__asm__(".ascii \"iecfile \\\"Logical/Libraries/Acp10_MC/acp10_mc.fun\\\" scope \\\"global\\\"\\n\"");
+
+/* Exported library functions and function blocks */
+
+__asm__(".previous");
+
+
+#endif /* _BUR_1621514705_8_ */
+
